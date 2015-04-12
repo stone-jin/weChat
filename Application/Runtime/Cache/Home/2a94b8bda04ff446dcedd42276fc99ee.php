84e23996ce999b6ec658dab3389592f6c2f77fd7
stone-jin <jb1520006273@gmail.com>
2015-04-12 18:41:56 +0800

    BugID: 000000 Description: 修改了路径

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,30 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset=utf8>
+	<title>拾石而砌 首页</title>
+	<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+	<link rel="stylesheet" type="text/css" href="/myWeChat/weChat/weixin2/Public/Home/css/index.css?2015_4_12_8">
+	<script type="text/javascript" src="http://cdn.bootcss.com/jquery/2.1.3/jquery.js"></script>
+</head>
+<body>
+	<div class="top">
+		
+	</div>
+	<div class="test">
+		<div class="test_block">
+			<span class="test_block_nav" hidefocus="true">生日</span>
+		</div>
+		<div class="test_block">
+			<span class="test_block_nav" hidefocus="true">短信祝福</span>
+		</div>
+		<div class="test_block">
+			<span class="test_block_nav" hidefocus="true">个人中心</span>
+		</div>
+		<div class="test_block">
+			<span class="test_block_nav" hidefocus="true">更多</span>
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="/myWeChat/weChat/weixin2/Public/Home/js/index.js"></script>
+</html>
