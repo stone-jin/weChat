73ccad160d0d420d291bc48f65c7bdcc1c6a6ebb
stone-jin <jb1520006273@gmail.com>
2015-04-14 23:51:21 +0800

    Bug ID: 00000 Description: 把网页给废了，界面是我软肋，怎么破

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -7,65 +7,32 @@
 	<link rel="stylesheet" type="text/css" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<script type="text/javascript" src="http://cdn.bootcss.com/jquery/2.1.3/jquery.js"></script>
 	<script type="text/javascript" src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="/myWeChat/weChat/Public/Home/css/index.css?2015_4_13_13">
+	<link rel="stylesheet" type="text/css" href="/myWeChat/weChat/Public/Home/css/index.css">
 </head>
 <body>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="span12">
-				<div class="carousel slide" id="carousel-451661">
-					<ol class="carousel-indicators">
-						<li data-slide-to="0" data-target="#carousel-451661">
-						</li>
-						<li data-slide-to="1" data-target="#carousel-451661" class="active">
-						</li>
-						<li data-slide-to="2" data-target="#carousel-451661">
-						</li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="item">
-							<img alt="" src="/myWeChat/weChat/Public/Home/image/1.jpg" />
-							<div class="carousel-caption">
-								<h4>
-									棒球
-								</h4>
-								<p>
-									棒球运动是一种以棒打球为主要特点，集体性、对抗性很强的球类运动项目，在美国、日本尤为盛行。
-								</p>
-							</div>
-						</div>
-						<div class="item active">
-							<img alt="" src="/myWeChat/weChat/Public/Home/image/2.jpg" />
-							<div class="carousel-caption">
-								<h4>
-									冲浪
-								</h4>
-								<p>
-									冲浪是以海浪为动力，利用自身的高超技巧和平衡能力，搏击海浪的一项运动。运动员站立在冲浪板上，或利用腹板、跪板、充气的橡皮垫、划艇、皮艇等驾驭海浪的一项水上运动。
-								</p>
-							</div>
-						</div>
-						<div class="item">
-							<img alt="" src="/myWeChat/weChat/Public/Home/image/3.jpg" />
-							<div class="carousel-caption">
-								<h4>
-									自行车
-								</h4>
-								<p>
-									以自行车为工具比赛骑行速度的体育运动。1896年第一届奥林匹克运动会上被列为正式比赛项目。环法赛为最著名的世界自行车锦标赛。
-								</p>
-							</div>
-						</div>
-					</div> <a data-slide="prev" href="#carousel-451661" class="left carousel-control">‹</a> <a data-slide="next" href="#carousel-451661" class="right carousel-control">›</a>
-				</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="test">
+				
 			</div>
 		</div>
-		<ul class="nav nav-tabs navbar-fixed-bottom test">
-			<li role="presentation"><a href="#">资讯</a></li>
-			<li role="presentation"><a href="#">开发经验</a></li>
-			<li role="presentation"><a href="#">娱乐</a></li>
-			<li role="presentation"><a href="#">个人中心</a></li>
-		</ul>
 	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<form>
+				<div class="form-group">
+					<label>Email Address</label>
+					<input type="email" class="form-control" placeholder="Enter email">
+				</div>
+				<div class="form-group">
+					<label>Password</label>
+					<input type="password" class="form-control" placeholder="Enter Password">
+				</div>
+				<button type="button" class="btn btn-default">Submit</button>
+			</form>
+		</div>
+	</div>
+</div>
 </body>
 </html>
