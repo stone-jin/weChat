839432972a794548bba177ad45f3525598d703f0
stone-jin <jb1520006273@gmail.com>
2015-04-14 22:51:29 +0800

    Bug ID: 00000 Description: 昨天多传了份东西

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,34 +1,71 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<html>
+<html lang="zh-CN">
 <head>
 	<meta charset=utf8>
 	<title>拾石而砌</title>
 	<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
-	<link rel="stylesheet" type="text/css" href="/myWeChat/weChat/Public/Home/css/index.css?2015_4_12_10">
+	<link rel="stylesheet" type="text/css" href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<script type="text/javascript" src="http://cdn.bootcss.com/jquery/2.1.3/jquery.js"></script>
+	<script type="text/javascript" src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="/myWeChat/weChat/Public/Home/css/index.css?2015_4_13_13">
 </head>
 <body>
-	<div class="top">
-		
-	</div>
-	<div class="test">
-		<div class="test_block">
-			<img src="/myWeChat/weChat/Public/Home/image/contact_after.png">
-			<a class="test_block_nav" id="current_page">首页</a>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="span12">
+				<div class="carousel slide" id="carousel-451661">
+					<ol class="carousel-indicators">
+						<li data-slide-to="0" data-target="#carousel-451661">
+						</li>
+						<li data-slide-to="1" data-target="#carousel-451661" class="active">
+						</li>
+						<li data-slide-to="2" data-target="#carousel-451661">
+						</li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="item">
+							<img alt="" src="/myWeChat/weChat/Public/Home/image/1.jpg" />
+							<div class="carousel-caption">
+								<h4>
+									棒球
+								</h4>
+								<p>
+									棒球运动是一种以棒打球为主要特点，集体性、对抗性很强的球类运动项目，在美国、日本尤为盛行。
+								</p>
+							</div>
+						</div>
+						<div class="item active">
+							<img alt="" src="/myWeChat/weChat/Public/Home/image/2.jpg" />
+							<div class="carousel-caption">
+								<h4>
+									冲浪
+								</h4>
+								<p>
+									冲浪是以海浪为动力，利用自身的高超技巧和平衡能力，搏击海浪的一项运动。运动员站立在冲浪板上，或利用腹板、跪板、充气的橡皮垫、划艇、皮艇等驾驭海浪的一项水上运动。
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img alt="" src="/myWeChat/weChat/Public/Home/image/3.jpg" />
+							<div class="carousel-caption">
+								<h4>
+									自行车
+								</h4>
+								<p>
+									以自行车为工具比赛骑行速度的体育运动。1896年第一届奥林匹克运动会上被列为正式比赛项目。环法赛为最著名的世界自行车锦标赛。
+								</p>
+							</div>
+						</div>
+					</div> <a data-slide="prev" href="#carousel-451661" class="left carousel-control">‹</a> <a data-slide="next" href="#carousel-451661" class="right carousel-control">›</a>
+				</div>
+			</div>
 		</div>
-		<div class="test_block">
-			<img src="/myWeChat/weChat/Public/Home/image/bless.png">
-			<a class="test_block_nav">开发指导</a>
-		</div>
-		<div class="test_block">
-			<img src="/myWeChat/weChat/Public/Home/image/colorful.png">
-			<a class="test_block_nav">娱乐专区</a>
-		</div>
-		<div class="test_block">
-			<img src="/myWeChat/weChat/Public/Home/image/center_selected.png">
-			<a class="test_block_nav">个人中心</a>
-		</div>
+		<ul class="nav nav-tabs navbar-fixed-bottom test">
+			<li role="presentation"><a href="#">资讯</a></li>
+			<li role="presentation"><a href="#">开发经验</a></li>
+			<li role="presentation"><a href="#">娱乐</a></li>
+			<li role="presentation"><a href="#">个人中心</a></li>
+		</ul>
 	</div>
 </body>
-<script type="text/javascript" src="/myWeChat/weChat/Public/Home/js/index.js?2015_4_12_5"></script>
 </html>
